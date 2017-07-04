aff9a895bed38c0d8d2432c3ac86936f6a96dd0e
sathyaveer <sathya.dveer@gmail.com>
2017-07-04 12:47:40 -0400

    added customer life time value calculation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Shutterfly-code-challenge
+# Shutterfly-code-challenge: Calculation of simple Customer life time value and finding top top customers.
+
+This repo contains python code that calculates customer life time value of each customer and returns top two customers based on customer life time value.
+Note: This code can be used to return top ‘X’ (X can be any number) customers. In my input data, I have entries for five customers and returning top two customers. 
+Total number of weeks (by considering global start and end times) in given a data set is four (4).
+
+Software requirements, packages, execution:
+
+This code is written in python 2.7 language. This code uses three standard python libraries: pandas, json, datetime. These libraries come with python 2.7. If there is any problem with unavailability of these libraries, please execute below command on command line.  Pip install library-name. Ex: pip install pandas.
+Input data required for this program is there in ‘input’ folder with the name ‘input.json’.
+Please execute this code either using command line: python name_of_the_module.py OR any IDE such as spyder, pycharm.
+
+Design considerations & Assumptions:
+•	Every event should have 'type' present. If type is empty, program will not consider that event. This design consideration is important as we should consider right type of events while calculating customer life time value and in future you may have some other events included in your input data set.
+•	For event type 'SITE_VISIT', if there are multiple entries with same key, event_time, customer_id combination, program will consider those entries as redundant and keeps only one entry. I assume system sometimes writes same event multiple times because of some issues.
+•	For event type 'SITE_VISIT', if customer_id is missing, program will discard that entry and will not be taken into consideration for further analysis.
+•	For event type 'ORDER', if customer_id is missing, program will discard that entry and will not be taken into consideration for further analysis.
+•	Customer can have only site visits and not have any orders in for a timeframe. So, program assign zero value to total expenditures.
+•	For event type ‘ORDER’, if the value of ‘verb’ is UPDATE for order id, consider the latest total_amount of this updated order id based on time stamp (between timestamps of NEW and UPDATE order id, timestamp of UPDATE is latest). Remove the entry of NEW order for the same order_id. If there are multiple UPDATEs on same order id, consider the latest UPDATE.
+There are two cases.
+1)	For a given order id, there can be following verbs: NEW, UPDATE, UPDATE…In this case consider only last update (discard all previous UPDATEs and NEW).
+2)	For a given order id, there can be only “NEW” verb in given time frame. Consider that ‘NEW’ entry.
+•	Program assume that each ‘new order ‘will have only one entry in input data set.
+
+Included all these exceptional scenarios as well in my test input data to test functionality of my code. 
+
+Ouput result:
+Output of program place in output folder. It has two files.
+1)top_two_customers.csv: It has infomration about top tow customers with highest customer life time value.
+2)customer_ltv.csv: It has information about all the customers with all the details such as CustomerId, Visits, TotalExpenditure, NumberOfWeeks, ExpenditurePerVisit, SiteVisitPerWeek, Customer life time value.
+
--- src/customer_life_time.py
+++ src/customer_life_time.py
@@ -14,14 +14,24 @@ import json
 import pandas as pd
 
 
+N = 2     # number of top customers that this program has to return 
+AVG_CUST_LIFESPAN = 10
+
 def main():
-    order, visit = input_process()
-    customers = simple_ltv_customers(order, visit)
-    print customers
-    
-def input_process():
     """
-    This method reads the input data from json file from local directory 
+    This is main function and entery point for the entire script. It calls other
+    functions to filter data, to preprocess data, and to calculate simple
+    customer life time value. At the end stores top customers in a file.
+    """
+    
+    order, visit = ingest()
+    order_final, visit_final = pre_process(order, visit)
+    top_customers = top_two_simple_ltv_customers(order_final, visit_final)
+    top_customers.to_csv(r'D:\Shutterfly-code-challenge\output\top_two_customers.csv', index=False)    
+    
+def ingest():
+    """
+    This function reads the input data from json file from local directory
     location and filters the input json data based on events(orders, visits, image upload, customer).
     In this case data are filterd by evnts orders and visits.
     
@@ -31,67 +41,121 @@ def input_process():
     
     """
     order_list = []
-    site_visit_list =[]
-    with open(r'D:\Shutterfly\input\input.json') as input_data:
+    site_visit_list = []
+    with open(r'D:\Shutterfly-code-challenge\input\input.json') as input_data:
         json_data = json.load(input_data)
     for event in json_data:
         if event['type'] == 'ORDER':
-            event_time = datetime.strptime(str(event['event_time']), '%Y-%m-%dT%H:%M:%S.%fZ')
-            amount = float(event['total_amount'].split(' ')[0])       # separates amount from USD, and takes only amount
-            week_number = int(event_time.strftime('%U'))        #returns week number of event time
-            order_list.append([str(event['customer_id']), event_time, amount, week_number])
+            if event['customer_id'] != ' ':             #considers record only if customer id is present
+                event_time = datetime.strptime(str(event['event_time']), '%Y-%m-%dT%H:%M:%S.%fZ')
+                amount = float(event['total_amount'].split(' ')[0])       # separates amount from USD, and takes only amount
+                week_number = int(event_time.strftime('%U'))        #returns week number of event time
+                order_list.append([str(event['customer_id']), event['verb'], event['key'], event_time, amount, week_number])
         if event['type'] == 'SITE_VISIT':
-            event_time = datetime.strptime(str(event['event_time']), '%Y-%m-%dT%H:%M:%S.%fZ')
-            week_number = int(event_time.strftime('%U'))        #returns week number of event time
-            visit = 1               #number for each visit to site 
-            site_visit_list.append([str(event['customer_id']), event_time, week_number, visit])
+            if event['customer_id'] != ' ':             #considers record only if customer id is present
+                event_time = datetime.strptime(str(event['event_time']), '%Y-%m-%dT%H:%M:%S.%fZ')
+                week_number = int(event_time.strftime('%U'))       #returns week number of event time
+                visit = 1              #number for each visit to site 
+                site_visit_list.append([str(event['customer_id']), event['key'], event_time, week_number, visit])
         else:
             pass
-    
-    order_df = pd.DataFrame(order_list, columns=['CustomerId', 'EventTime', 'Amount', 'WeekNumber'])
-    site_visit_df = pd.DataFrame(site_visit_list, columns=['CustomerId', 'EventTime', 'WeekNumber', 'Visits'])
+    order_df = pd.DataFrame(order_list, columns=['CustomerId', 'Verb', 'OrderId', 'EventTime', 'Amount', 'WeekNumber'])
+    site_visit_df = pd.DataFrame(site_visit_list, columns=['CustomerId', 'PageId', 'EventTime', 'WeekNumber', 'Visits'])
     
     return order_df, site_visit_df
-    
-def simple_ltv_customers(order_df, site_visit_df):
+
+def pre_process(order_df, visit_df):
     """
-    This method calculates life time value of each customer and 
-    return top two customer id
+    This function does preprocessing on order and visit data. If there are any 
+    updates on order id, considers most recent transaction and ignoring previous
+    transactions. If there areany redundant entries in site visit data, function
+    keeps one entry and deletes others.
     
     Input:
     * order_df(dataframe): dataframe that has data about customer transactions such as
+                           customer_id, nature of order(NEW/UPDATE),order id,time of transaction,
+                           amount they spent, week number in which given time of transaction falls.
+    * visit_df(dataframe): dataframe has data about customer visits to website such as 
+                            custommer id, page id, time visited, week number in
+                            which given time visited falls, count for each visit
+                                
+    Output:
+    * order_df, visit_df dataframes after preprocessing.
+    
+    """
+    
+    # considers recent updated entry for a given order id if there are any.
+    order_df = order_df.sort_values(by=['CustomerId', 'OrderId', 'EventTime'], ascending=True)
+    #print order_df
+    length = len(order_df)
+    i = 0
+    while i <= length:
+        if i == length - 1:
+            break
+        elif order_df.loc[i]['OrderId'] == order_df.loc[i+1]['OrderId']:
+            if order_df.loc[i]['Verb'] == 'NEW' and order_df.loc[i+1]['Verb'] == 'UPDATE':
+                order_df.drop(order_df.index[i], inplace=True)
+                order_df = order_df.reset_index(drop=True)
+                i = i - 1
+                length = length - 1
+                continue
+            elif order_df.loc[i]['Verb'] == 'UPDATE' and order_df.loc[i+1]['Verb'] == 'UPDATE':
+                order_df.drop(order_df.index[i], inplace=True)
+                order_df = order_df.reset_index(drop=True)
+                i = i - 1
+                length = length - 1
+                continue
+        else:
+            i = i + 1
+            continue
+    
+    #removes dupliactes (from site visit data) except first occuurence based on columns customer_id, page id, event time
+    visit_df.drop_duplicates(subset=['CustomerId', 'PageId', 'EventTime'], keep='first', inplace=True)
+
+    return order_df, visit_df
+            
+def top_two_simple_ltv_customers(order_final, site_visit_final):
+    """
+    This function calculates life time value of each customer and 
+    return top two customer id
+    
+    Input:
+    * order_final(dataframe): dataframe that has data about customer transactions such as
                            customer_id, time of transaction, amount they spent, week number in
                            which given time of transaction falls.
-    * site_visit_df(dataframe): dataframe has data about customer visits to website such as 
+    * site_visit_final(dataframe): dataframe has data about customer visits to website such as 
                                 custommer id, time visited, week number in
                                 which given time visited falls, count for each visit
                                 
     Output:
-    * Customer id(list): list of customer ids with highest life time value
-    
+    * Top two Customers(dataframe): list of customer ids, respective life time value amount. 
     """
-    avg_cust_lifespan = 10
-    no_of_weeks = max(site_visit_df['WeekNumber']) - min(site_visit_df['WeekNumber']) + 1
+    
+    #calculates number of weeks in entire data
+    num_of_weeks = max(site_visit_final['WeekNumber']) - min(site_visit_final['WeekNumber']) + 1 
     
     # calculates total expenditure of each customer in a given timeframe.
     # The resultant dataframe has two columns: CustomerId, Amount.
-    order_ser = order_df.groupby(['CustomerId'])['Amount'].sum()   
-    order_df = order_ser.reset_index()
+    order_ser = order_final.groupby(['CustomerId'])['Amount'].sum()   
+    order_final = order_ser.reset_index()
     
     # calculates number of customer visits of each customer in a given timeframe.
     # The resultant dataframe has two columns: CustomerId, Visits.
-    site_visit_ser = site_visit_df.groupby('CustomerId')['Visits'].count()   
-    site_visit_df = site_visit_ser.reset_index()
+    site_visit_ser = site_visit_final.groupby('CustomerId')['Visits'].count()   
+    site_visit_final = site_visit_ser.reset_index()
     
-    visit_order_df = pd.merge(site_visit_df, order_df, on = 'CustomerId', how='left')
+    visit_order_df = pd.merge(site_visit_final, order_final, on ='CustomerId', how='left')
     visit_order_df.fillna(0, inplace=True)      #useful when customer has visited site but no orders in a given timeframe
+    visit_order_df['NumberOfWeeks'] = num_of_weeks
     visit_order_df['ExpenditurePerVisit'] = visit_order_df['Amount'] / visit_order_df['Visits']
-    visit_order_df['SiteVisitPerWeek'] = visit_order_df['Visits'] / no_of_weeks
+    visit_order_df['SiteVisitPerWeek'] = visit_order_df['Visits'] / num_of_weeks
     visit_order_df['CustomerLTV'] = visit_order_df['ExpenditurePerVisit'] * visit_order_df['SiteVisitPerWeek']
-    visit_order_df['CustomerLTV'] = visit_order_df['CustomerLTV'] * 52 * avg_cust_lifespan
-    visit_order_df.sort_values(by = 'CustomerLTV', inplace=True, ascending=False)
+    visit_order_df['CustomerLTV'] = visit_order_df['CustomerLTV'] * 52 * AVG_CUST_LIFESPAN
+    visit_order_df.sort_values(by='CustomerLTV', inplace=True, ascending=False)
+    visit_order_df.rename(columns={'Amount': 'TotalExpenditure'}, inplace=True)
+    visit_order_df.to_csv('D:\Shutterfly-code-challenge\output\customer_ltv.csv', index=False)  
     
-    return list(visit_order_df.head(2)['CustomerId'])
+    return visit_order_df.head(N)[['CustomerId', 'CustomerLTV']]
     
     
 if __name__ == "__main__":
